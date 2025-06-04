ae6e63b36558fa4802e854cc27566c83037e9be9
Mateo Astudillo <ast.mateo@gmail.com>
2025-06-04 00:45:09 -0300

    ejercicio

--- ejercicio/habitacion-3/puerta-2/.oculto.php
+++ ejercicio/habitacion-3/puerta-2/.oculto.php
@@ -1,2 +1,5 @@
 <?php
     echo "Puede que te haya mentido...\n";
+    echo "Como recompensa te doy un comando para\n";
+    echo "filtrar el archivo largo\n";
+    echo "grep pista archivo-largo.txt\n";
